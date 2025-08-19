ee97741828c8978877d49aedc9fbc783767efdb3
hungnq <quanghungnguyen2004@gmail.com>
2025-08-20 00:20:07 +0700

    fix inngest error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -10,7 +10,7 @@ const syncUserCreation = inngest.createFunction(
     {event: 'clerk/user.created'},
     async ({event})=> {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_addresses.split('@')[0]
+        let username = email_addresses[0].email_address.split('@')[0]
 
         // Check availability of username
         const user = await User.findOne({username})
@@ -21,7 +21,7 @@ const syncUserCreation = inngest.createFunction(
 
         const userData = {
             _id: id,
-            email:email_addresses[0].email_addresses,
+            email:email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture:image_url,
             username
@@ -38,7 +38,7 @@ const syncUserUpdation = inngest.createFunction(
         const {id, first_name, last_name, email_addresses, image_url} = event.data
 
         const updatedUserData = {
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture:image_url
         }
